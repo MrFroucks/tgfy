e49d31ee0e5424722e6d63debb3f1789c44d1835
MrFroucks <69039573+MrFroucks@users.noreply.github.com>
2024-04-20 08:39:29 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -2,22 +2,26 @@ import sqlite3
 
 class Database(object):
 	def __init__(self, db_file):
+		# объявление переменных, для работы с базой данных
 		self.connection = sqlite3.connect(db_file)
 		self.cursor = self.connection.cursor()
 
+		# проверка на наличие пользователя в базе данных
 	def user_exists(self,user_id):
 		with self.connection:
 			result = self.cursor.execute('select * from users where user_id = ?',(user_id,)).fetchall()
 			return bool(len(result))
 
+		# добавление пользователя в базу данных
 	def add_user(self,user_id,lang):
 		with self.connection:
 			return self.cursor.execute('insert into users (user_id, lang) values (?,?)',(user_id,lang,))
-
+		# обновление языка пользователя
 	def update_user(self,user_id,lang):
 		with self.connection:
 			return self.cursor.execute('update users set lang == ? where user_id = ?',(lang,user_id,))
 
+		# считывание языка пользователя
 	def get_lang(self,user_id):
 		with self.connection:
 			return self.cursor.execute('select lang from users where user_id = ?',(user_id,)).fetchone()[0]
--- markups.py
+++ markups.py
@@ -3,6 +3,9 @@ from aiogram import Bot,Dispatcher,executor,types
 from localisations import ll
 
 
+# -------------------------------------------------------------------------------
+# МЕНЮ ЯЗЫКОВ
+# -------------------------------------------------------------------------------
 langMenu = InlineKeyboardMarkup(row_width=3)
 langEN = InlineKeyboardButton(text='English',callback_data='lang_en')
 langES = InlineKeyboardButton(text='Español',callback_data='lang_es')
@@ -10,19 +13,57 @@ langRU = InlineKeyboardButton(text='Русский',callback_data='lang_ru')
 langMenu.add(langEN,langES,langRU)
 
 
+# -------------------------------------------------------------------------------
+# МЕНЮ БЛОКОВ С ПЕРЕВОДОМ
+# -------------------------------------------------------------------------------
 def blocksMenu(lang):
-	blocksMenu = InlineKeyboardMarkup(row_width=2)
+	blocksMenu = InlineKeyboardMarkup(row_width=3)
 	block1 = InlineKeyboardButton(text=ll('🍾Культура и тенденции',lang),callback_data='cultura')
 	block2 = InlineKeyboardButton(text=ll('👾Навигация',lang),callback_data='nav')
 	block3 = InlineKeyboardButton(text=ll('🦻Словарь',lang),callback_data='dic')
 	block4 = InlineKeyboardButton(text=ll('👀Общество',lang),callback_data='obs')
 	block5 = InlineKeyboardButton(text=ll('⭐️Повседневная жизнь',lang),callback_data='dlf')
 	block6 = InlineKeyboardButton(text=ll('ПОМЕНЯТЬ ЯЗЫК',lang),callback_data='LNG')
-	blocksMenu.add(block1,block2,block3,block4,block5,block6)
+	block7 = InlineKeyboardButton(text=ll('ПЛАТНАЯ ПОДПИСКА',lang),callback_data='VIP')
+	blocksMenu.add(block2,block3,block5,block6)
 	return blocksMenu
 
+
+
+# -------------------------------------------------------------------------------
+# МЕНЮ СЛОВАРЯ
+# -------------------------------------------------------------------------------
+def divMenu(lang):
+	divMenu = InlineKeyboardMarkup(row_width=2)
+	dm1 = InlineKeyboardButton(text=ll('Жаргонизмы',lang),callback_data='jar')
+	dm3 = InlineKeyboardButton(text=ll('Крылатые выражения',lang),callback_data='krl')
+	divMenu.add(dm1,dm3)
+	return divMenu
+def BdivMenu(lang):
+	BdivMenu = InlineKeyboardMarkup()
+	Bdm1 = InlineKeyboardButton(text=ll('🦻Словарь',lang),callback_data='dic')
+	BdivMenu.add(Bdm1)
+	return BdivMenu
+
+def trnMenu(lang):
+	trnMenu = InlineKeyboardMarkup(row_width=2)
+	trn1 = InlineKeyboardButton(text=ll('Транспорт',lang),callback_data='trn')
+	trn2 = InlineKeyboardButton(text=ll('ChatGPT (soon...)'),callback_data='gpt')
+	trn3 = InlineKeyboardButton(text=ll('Общественный транспорт',lang),callback_data='otrn')
+	trnMenu.add(trn1,trn3)
+	return trnMenu
+def BtrnMenu(lang):
+	BtrnMenu = InlineKeyboardMarkup()
+	Btrn1 = InlineKeyboardButton(text=ll('👾Навигация',lang),callback_data='nav')
+	BtrnMenu.add(Btrn1)
+	return BtrnMenu
+
+
+# -------------------------------------------------------------------------------
+# КЛАВИАТУРА ДЛЯ ВОЗВРАЩЕНИЯ НАЗАД
+# -------------------------------------------------------------------------------
 def backBlocks(lang):
 	backBlocksMenu = ReplyKeyboardMarkup(resize_keyboard=True)
-	backBlocksBtn = KeyboardButton(ll('Блоки',lang))
+	backBlocksBtn = KeyboardButton('/blocks')
 	backBlocksMenu.add(backBlocksBtn)
 	return backBlocksMenu
